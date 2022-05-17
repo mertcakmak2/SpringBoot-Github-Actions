196578aa9ab42210e3e9fb5ca8028eadc982c8cc
mertcakmak2 <mertcakmak2@gmail.com>
2022-05-17 14:12:55 +0300

    github actions

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM adoptopenjdk/openjdk11:alpine
+EXPOSE 8080
+COPY target/github-spring-app.jar github-spring-app.jar
+ENTRYPOINT ["java","-jar","/github-spring-app.jar"]
---
+++ src/test/java/com/github/actions/ActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
